bf25014b9dc6103b5d5601c63afdf0462b7ccb33
Janice Kennedy <137051970+0921-janice@users.noreply.github.com>
2026-07-19 21:47:28 -0500

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.py
@@ -0,0 +1,74 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = fast = head
+
+        #find middle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        #reverse second half
+        cur, prev = slow.next, None
+        slow.next = None
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+
+        #merge
+        l1,l2 = head, prev
+
+        while l1 and l2:
+            temp1 = l1.next
+            temp2 = l2.next
+            l1.next = l2
+            l2.next = temp1
+            l1 = temp1
+            l2 = temp2
+
+
+
+
+
+        # fast = slow = head
+
+        # while fast and fast.next:
+        #     fast = fast.next.next
+        #     slow = slow.next
+        
+        # #reverse second half
+        # cur, prev = slow.next, None
+        # slow.next = None
+
+        # while cur:
+        #     temp = cur.next
+        #     cur.next = prev
+        #     prev = cur
+        #     cur = temp
+
+        # #merge lists
+        # l1,l2 = head, prev
+
+        # while l1 and l2:
+        #     temp1 = l1.next
+        #     temp2 = l2.next
+        #     l1.next = l2
+        #     l2.next = temp1
+        #     l1 = temp1
+        #     l2 = temp2
+    
+
+
+
+
+
+
+
+
